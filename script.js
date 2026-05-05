61aec03a73795145be5cd37839278986a5427fa6
toms38 <tariqaziz32492@gmail.com>
2026-05-06 00:46:07 +0500

    TOSS

---
+++ script.js
@@ -0,0 +1,19 @@
+let heading = document.querySelector("h1");
+    let btn = document.getElementById("tossBtn");
+    let coin = document.getElementById("coin");
+
+    btn.addEventListener("click", () => {
+    coin.classList.add("roll");
+
+    setTimeout(() => {
+    coin.classList.remove("roll");
+    let random = Math.ceil(Math.random() * 2);
+    if (random === 1) {
+      coin.src = "imgs/head.png";   // HEAD image
+      heading.innerText = "HEAD 😍";
+    } else {
+      coin.src = "imgs/tail.png";   // TAIL image
+      heading.innerText = "TAIL 😜";
+    }
+  }, 1000);  
+});
